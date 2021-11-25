afc96ade6924348d8c89a3195733620c7cea6e2a
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2021-11-25 17:35:07 +0100

    initial commit
    Signed-off-by: Benedikt Bergenthal <benedikt.bergenthal@imflow.me>

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/ImFlow/knocken
+
+go 1.17
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	google.golang.org/protobuf v1.26.0-rc.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ knocken.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sergi/go-diff/diffmatchpatch"
+	"gopkg.in/yaml.v2"
+)
+
+type URL []struct {
+	Targets []string `yaml:"targets"`
+}
+
+var (
+	statSame = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "imflow",
+			Subsystem: "knocken",
+			Name:      "same",
+			Help:      "Percentage of same HTML code on a website in the last 5 min.",
+		},
+		[]string{
+			"target",
+		},
+	)
+)
+
+func getHTML(url string) ([]byte, error) {
+	resp, err := http.Get("https://" + url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+func writeFile(fileName string, data []byte) error {
+	return ioutil.WriteFile("./html/"+fileName, data, 0644)
+}
+
+func getContentOfFileIfExists(fileName string) ([]byte, error) {
+	if _, err := ioutil.ReadFile("./html/" + fileName); err != nil {
+		return nil, err
+	}
+	return ioutil.ReadFile("./html/" + fileName)
+}
+
+func recordMetrics(URLs URL) {
+	go func() {
+		for {
+			// Read file to byte array
+			// data, err := ioutil.ReadFile("targets.yml")
+			// if err != nil {
+			// 	panic(err)
+			// }
+
+			for _, url := range URLs {
+				for _, target := range url.Targets {
+					fmt.Println("Checking: " + target)
+					dmp := diffmatchpatch.New()
+					htmlNew, err := getHTML(target)
+					if err != nil {
+						panic(err)
+					}
+					htmlOld, err := getContentOfFileIfExists(target)
+
+					if err != nil {
+						writeFile(target, htmlNew)
+						continue
+					}
+					htmlNewStr := string(htmlNew)
+					htmlOldStr := string(htmlOld)
+					diffs := dmp.DiffMain(htmlNewStr, htmlOldStr, false)
+					levenshteinDiff := float64(dmp.DiffLevenshtein(diffs))
+					len1 := float64(len(htmlNewStr))
+					len2 := float64(len(htmlOldStr))
+					weightedLen := (len1 + len2) / 2.0
+					same := 1 - (levenshteinDiff / weightedLen)
+					fmt.Println(same)
+					statSame.WithLabelValues(target).Set(same)
+				}
+			}
+			time.Sleep(5 * time.Minute)
+		}
+	}()
+}
+
+func main() {
+	// url := "https://imflow.me"
+	fmt.Println("Starting...")
+
+	data, err := ioutil.ReadFile("targets.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	var URLs URL
+	err = yaml.Unmarshal(data, &URLs)
+	if err != nil {
+		panic(err)
+	}
+
+	prometheus.MustRegister(statSame)
+	recordMetrics(URLs)
+
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":9101", nil))
+}
