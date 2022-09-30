93c14b8a50bf88ad2fab8687fc5061a09c79c392
Benedikt Bergenthal <benedikt@escsoftware.de>
2022-09-30 09:50:50 +0200

    added parsers to git ;)
    Signed-off-by: Benedikt Bergenthal <benedikt@escsoftware.de>

---
+++ parsers/targetParser.go
@@ -0,0 +1,43 @@
+package parsers
+
+import (
+	"io/ioutil"
+
+	"github.com/wutzi15/knocken/types"
+	"gopkg.in/yaml.v2"
+)
+
+func ParseTargets(targets string) (types.URL, error) {
+	data, err := ioutil.ReadFile(targets)
+	if err != nil {
+		return types.URL{}, err
+	}
+
+	var URLs types.URL
+	err = yaml.Unmarshal(data, &URLs)
+	if err != nil {
+		return types.URL{}, err
+	}
+	return URLs, nil
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+func RemoveIgnoredTargets(URLs types.URL, ignore types.URL) types.URL {
+	var newURLs types.URL
+	for i := 0; i < len(URLs.Targets); i++ {
+		if !contains(ignore.Targets, URLs.Targets[i]) {
+			newURLs.Targets = append(newURLs.Targets, URLs.Targets[i])
+		}
+	}
+
+	return URLs
+}
---
+++ parsers/targetParser_test.go
@@ -0,0 +1,53 @@
+package parsers_test
+
+import (
+	"testing"
+
+	"github.com/wutzi15/knocken/parsers"
+)
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+func TestTargetParser(t *testing.T) {
+	var URLs, err = parsers.ParseTargets("../targets.sample.yml")
+	if err != nil {
+		t.Errorf("Error parsing targets: %v", err)
+	}
+	if len(URLs.Targets) != 2 {
+		t.Errorf("Expected 2 but got %+v", URLs.Targets)
+	}
+	if !contains(URLs.Targets, "google.com") {
+		t.Errorf("Expected to find google.com but did not %v", URLs.Targets)
+	}
+	if !contains(URLs.Targets, "escsoftware.de") {
+		t.Errorf("Expected to find escsoftware.de but did not %v", URLs.Targets)
+	}
+}
+
+func TestIgnoreTargets(t *testing.T) {
+	URLs, err := parsers.ParseTargets("../targets.sample.yml")
+	if err != nil {
+		t.Errorf("Error parsing targets: %v", err)
+	}
+
+	ignore, err := parsers.ParseTargets("../ignore.sample.yml")
+	if err != nil {
+		t.Errorf("Error parsing ignore: %v", err)
+	}
+	URLs = parsers.RemoveIgnoredTargets(URLs, ignore)
+	if len(URLs.Targets) != 1 {
+		t.Errorf("Expected 1 but got %+v", len(URLs.Targets))
+	}
+	if !contains(URLs.Targets, "escsoftware.de") {
+		t.Errorf("Expected to find escsoftware.de but did not %v", URLs.Targets)
+	}
+
+}
